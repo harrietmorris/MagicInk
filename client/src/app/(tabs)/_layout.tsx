8102b2a9a44c911ef7504b838879223b251952e9
Regina Czech <regina.czech@yahoo.com>
2024-06-22 11:57:46 +0200

    fix: added expo dev dependencies to app

--- client/src/app/(tabs)/_layout.tsx
+++ client/src/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
+import 'expo-dev-client';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
