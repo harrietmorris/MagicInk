5d71004c06ab6152fbdc64ba107ca942c1948019
Regina Czech <regina.czech@yahoo.com>
2024-06-24 14:58:00 +0200

    fix: merge content in API service files

--- client/services/ApiService.tsx
+++ client/services/ApiService.tsx
@@ -1,29 +1,58 @@
-import { ProfileType, StoryType, UserType } from "../Types"
-import axios, {AxiosResponse} from "axios"; 
+import { ProfileType, StoryType, UserType } from '../Types';
+import axios, { AxiosResponse, isCancel, AxiosError } from 'axios';
 
-const API_URL = 'http://localhost:3000' //this may need to change to IP address
+const BASE_URL = 'http://localhost:3000'; //this may need to change to IP address
 
 export const axiosUser = async (id: number): Promise<UserType> => {
-    try {
-        const response: AxiosResponse<UserType> = await axios.get(`${API_URL}/users/${id}`);
-        console.log('getting user:', response.data)
-        return response.data
-    } catch (e) {
-        console.log('error getting user', e)
-        throw e
-    }
-} 
+  try {
+    const response: AxiosResponse<UserType> = await axios.get(`${BASE_URL}/users/${id}`);
+    console.log('getting user:', response.data);
+    return response.data;
+  } catch (e) {
+    console.log('error getting user', e);
+    throw e;
+  }
+};
 
-export const axiosAllProfiles = async (userId: number): Promise<ProfileType[]> => {
+// export const axiosAllProfiles = async (userId: number): Promise<ProfileType[]> => {};
 
-}
+// export const axiosSelectedProfile = async (profileId: number): Promise<ProfileType> => {};
 
-export const axiosSelectedProfile = async (profileId: number): Promise<ProfileType> => {
+// export const axiosStoriesByProfile = async (profileId: number): Promise<StoryType[]> => {};
 
-}
+export const axiosSelectedStory = async (storyId: number): Promise<StoryType> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/stories/${storyId}`);
+    console.log('🚀 ~ getStoryById ~ res:', response);
+    return response.data;
+  } catch (error) {
+      console.error('Error creating story', error);
+      throw error;
+  }
+};
 
-export const axiosStoriesByProfile = async (profileId: number): Promise<StoryType[]> => { }
+// export const axiosFavStories = async (profileId: number): Promise<StoryType[]> => {};
 
-export const axiosSelectedStory = async (storyId: number): Promise<StoryType> => { }
-
-export const axiosFavStories = async (profileId: number): Promise<StoryType[]> => { }
+export const createStory = async (
+  readingLevel: string,
+  location: string,
+  readingTime: number,
+  themes: string[],
+  simpleLanguage: boolean = false,
+  words: number[] = [],
+) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/story`, {
+      readingLevel,
+      location,
+      readingTime,
+      themes,
+      simpleLanguage,
+      words,
+    });
+    return response.data;
+  } catch (error) {
+      console.error('Error creating story', error);
+      throw error;
+  }
+};
