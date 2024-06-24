50ea21cdf8a167350603a4179b655f906f071e72
Regina Czech <regina.czech@yahoo.com>
2024-06-24 14:59:39 +0200

    fix: merge content in API service files

--- client/app/(tabs)/newStoryScreen.tsx
+++ client/app/(tabs)/newStoryScreen.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 import { Text, View, TextInput, Button, Alert } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import MultiSelectComponent from '@/components/MultiSelect';
-import { createStory } from '@/services/apiService';
+import { createStory } from '@/services/ApiService';
 import { FormData } from '@/Types';
 import { router } from 'expo-router';
 
@@ -76,7 +76,7 @@ export default function newStoryScreen () {
 
   return (
     <View style={styles.container}>
-      
+
       {/* TODO: Make this button functional */}
       <Button title='Suprise me!'/>
 
@@ -99,7 +99,7 @@ export default function newStoryScreen () {
         )}
       />
 
-      <Text style={styles.title}>How long do you want to read for?</Text>    
+      <Text style={styles.title}>How long do you want to read for?</Text>
       <Controller
         name="readingTime"
         control={control}
