ba33a73572ae35ef71e99f592b31047bccf1b423
Regina Czech <regina.czech@yahoo.com>
2024-06-25 17:04:59 +0200

    feat: google auth implemented in app/index screen

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,11 +1,90 @@
+import React, { useEffect } from 'react';
+import { Redirect } from 'expo-router';
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { useDataContext } from '@/context/globalContext';
 
-import React from 'react'
-import { Redirect } from 'expo-router'
-
-const index = () => {
-  return (
-    <Redirect href="/settingsScreen"></Redirect>
-  )
+interface userInfo {
+  id: string;
+  name: string | null;
+  email: string;
+  photo: string | null;
+  familyName: string | null;
+  givenName: string | null;
 }
 
-export default index
+const index = () => {
+  const { user, setUser } = useDataContext();
+
+  const configGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId: process.env.WEB_CLIENT_ID,
+      // androidClientId: process.env.ANDROID_CLIENT_ID,
+      // iosClientId: process.env.ANDROID_CLIENT_ID,
+    });
+  };
+
+  useEffect(() => {
+    configGoogleSignIn(); // will execute everytime the component mounts
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userResult = await GoogleSignin.signIn();
+      const userInfo = userResult.user;
+      setUser(userInfo);
+    } catch (err) {
+      //TODO: Add errors based on react-native-google status codes
+      console.log('Error', err);
+    }
+  };
+
+  //TODO: implement this logic
+  const checkIfUserIsValid = async () => {
+    try {
+      const userResult = await GoogleSignin.signInSilently();
+      const userInfo = userResult.user;
+      setUser(userInfo);
+    } catch (err) {
+      console.log('Error', err);
+    }
+  };
+
+  //TODO: move this functionality to the profile/ settings page & Logout button
+  const logout = () => {
+    setUser(undefined);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+  };
+
+  return (
+    <View style={styles.container}>
+      {user ? (
+        <>
+          <Redirect href='/profilesScreen'></Redirect>
+        </>
+      ) : (
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Standard}
+          color={GoogleSigninButton.Color.Light}
+          onPress={signIn}
+        />
+      )}
+      <Pressable onPress={logout}>
+        <Text>Logout</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default index;
