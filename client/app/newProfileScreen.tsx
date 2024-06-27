5781ddfec7e4fa85e74ac90a2faf4d5635dd25c8
Regina Czech <regina.czech@yahoo.com>
2024-06-27 21:09:38 +0200

    fix: style new profile Screen and buttons

--- client/app/newProfileScreen.tsx
+++ client/app/newProfileScreen.tsx
@@ -1,13 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import ProfileForm from '@/components/Profiles/ProfileForm'
+import { View } from 'react-native';
+import React from 'react';
+import ProfileForm from '@/components/Profiles/ProfileForm';
 
 const newProfile = () => {
   return (
-    <View>
-      <ProfileForm/>
+    <View className='m-8'>
+      <ProfileForm />
     </View>
-  )
-}
+  );
+};
 
-export default newProfile
+export default newProfile;
