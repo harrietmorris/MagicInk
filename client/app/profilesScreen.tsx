0b2bd2841a294114f966ac55004058e749b5aeda
Regina Czech <regina.czech@yahoo.com>
2024-06-27 17:34:31 +0200

    in progress - new profile button:

--- client/app/profilesScreen.tsx
+++ client/app/profilesScreen.tsx
@@ -1,43 +1,35 @@
-import { StyleSheet, SafeAreaView } from 'react-native'
-import React from 'react'
-import NewProfileButton from '../components/Profiles/NewProfileButton'
-import ListedProfiles from '@/components/Profiles/ListedProfiles'
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import NewProfileButton from '../components/Profiles/NewProfileButton';
+import ListedProfiles from '@/components/Profiles/ListedProfiles';
 
 const profilesScreen = () => {
-
-
   return (
-    <SafeAreaView style={styles.container}>
-
-      < NewProfileButton
-        route="/newProfileScreen"
-        buttonStyle={styles.buttonStyle}
-        textStyle={styles.textStyle} />
-
+    <View className='flex-1 flex-col items-center justify-center'>
+      <NewProfileButton route='/newProfileScreen' />
       <ListedProfiles />
-
-    </SafeAreaView>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   title: {
-    fontSize: 24,
-    marginBottom: 20,
+    // fontSize: 24,
+    // marginBottom: 20,
   },
   buttonStyle: {
-    backgroundColor: '#28a745',
-    padding: 15,
+    // backgroundColor: '#28a745',
+    // padding: 15,
   },
   textStyle: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    // fontSize: 18,
+    // fontWeight: 'bold',
   },
 });
 
-export default profilesScreen
+export default profilesScreen;
--- client/components/Profiles/NewProfileButton.tsx
+++ client/components/Profiles/NewProfileButton.tsx
@@ -1,40 +1,38 @@
-import { ViewStyle, TextStyle, TouchableOpacity, StyleSheet, Text, Pressable } from 'react-native'
-import React from 'react'
+import { ViewStyle, TextStyle, TouchableOpacity, StyleSheet, Text, Pressable } from 'react-native';
+import React from 'react';
 import { useRouter } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 interface NewProfileButtonProps {
-    route: string;
-    buttonStyle?: ViewStyle;
-    textStyle?: TextStyle; 
+  route: string;
 }
 
+function NewProfileButton({ route }: NewProfileButtonProps) {
+  const router = useRouter();
 
-function NewProfileButton({ route, buttonStyle, textStyle }: NewProfileButtonProps) {
-    const router = useRouter();
+  const reRouter = () => {
+    router.push(route);
+  };
 
-    const reRouter = () => {
-        router.push(route);
-    };
-
-    return (
-        <Pressable style={[styles.button, buttonStyle]} onPress={reRouter}>
-            <Text style={[styles.text, textStyle]}>Create new profile</Text>
-        </Pressable>
-    );
+  return (
+    <Pressable className='bg-dark-orange p-3 rounded-full' onPress={reRouter}>
+      <Text className='text-2xl text-white'>New Profile</Text>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#000000',
-        padding: 10,
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: '#fff',
-        fontSize: 16,
-    },
+  button: {
+    // backgroundColor: '#000000',
+    // padding: 10,
+    // borderRadius: 10,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  text: {
+    // color: '#fff',
+    // fontSize: 16,
+  },
 });
 
-export default NewProfileButton
+export default NewProfileButton;
