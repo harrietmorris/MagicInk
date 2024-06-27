307bc6c058fb4382860f5bce4d3c764d33a7f246
Regina Czech <regina.czech@yahoo.com>
2024-06-27 15:18:53 +0200

    colors added to theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      dark: vars({
+        '--bg': '#2B2936',
+        '--dark-orange': '#FF8052',
+        '--light-orange': '#F4A662',
+        '--yellow': '#F0E68F',
+        '--green': '#91EE91',
+        '--blue': '#4682B4',
+      }),
+      light: vars({
+        '--bg': '#FFFFFF',
+        '--dark-orange': '#FF8052',
+        '--light-orange': '#F4A662',
+        '--yellow': '#F0E68F',
+        '--green': '#91EE91',
+        '--blue': '#4682B4',
+      }),
+    },
   },
   plugins: [],
 };
+
+require ('./app/')
