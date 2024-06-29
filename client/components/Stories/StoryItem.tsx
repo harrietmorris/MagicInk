e2348a35478ee5bba3b5996d4667125c52af1189
Regina Czech <regina.czech@yahoo.com>
2024-06-29 16:47:07 +0200

    add delete story to story item

--- client/components/Stories/StoryItem.tsx
+++ client/components/Stories/StoryItem.tsx
@@ -1,28 +1,36 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from 'react-native';
+import React from 'react';
 import { StoryType } from '@/types';
 import FavButton from '../buttons/favButton';
+import DeleteStoryBtn from '../buttons/DeleteStoryBtn';
+import { styled } from 'nativewind';
 
 interface StoryItemProps {
-    story: StoryType;
-    handlePress: (story: StoryType) => void;
-    buttonColor: string;
+  story: StoryType;
+  handlePress: (story: StoryType) => void;
+  buttonColor: string;
 }
 
-const StoryItem= ({ story, handlePress, buttonColor } :StoryItemProps) => {
+const StyledPressable = styled(Pressable);
+const StyledView = styled(View);
 
-
-    return (
-        <View>
-            <Pressable className={`w-[140px] h-[140px] bg-${buttonColor} aspect-square rounded-lg ml-5 flex items-center justify-center`}
-                onPress={() => handlePress(story)} >
-                <Text className='text-base text-black text-center text-lg'>{story.title}</Text>
-                <View className='absolute bottom-1 left-1'>
-                    <FavButton storyId={story.id} />
-                </View>
-            </Pressable>
+const StoryItem = ({ story, handlePress, buttonColor }: StoryItemProps) => {
+  return (
+    <View>
+      <StyledPressable
+        className={`w-[200px] h-[200px] bg-${buttonColor} aspect-square rounded-lg ml-5 p-2 flex items-center justify-center`}
+        onPress={() => handlePress(story)}
+      >
+        <Text className='text-base text-black text-center text-lg font-bold'>{story.title}</Text>
+        <StyledView className='absolute top-2 left-2'>
+          <DeleteStoryBtn storyId={story.id} />
+        </StyledView>
+        <View className='absolute top-2 right-2'>
+          <FavButton storyId={story.id} />
         </View>
-    )
-}
+      </StyledPressable>
+    </View>
+  );
+};
 
-export default StoryItem
+export default StoryItem;
