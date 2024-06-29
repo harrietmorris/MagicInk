8551300538efe31e91158ad6107b441189ff215f
Harriet <Harriet.mae.morris@gmail.com>
2024-06-29 17:57:32 +0200

    better loading screen

--- client/app/loadingScreen.tsx
+++ client/app/loadingScreen.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Image } from 'expo-image';
 
+const gifs = [
+  require('../assets/images/hippo.gif'),
+  require('../assets/images/angrybird.gif'),
+  require('../assets/images/bear.gif'),
+  require('../assets/images/flying.gif'),
+];
+
+const getRandomGif = () => {
+  const randomIndex = Math.floor(Math.random() * gifs.length);
+  return gifs[randomIndex];
+};
 
 const LoadingScreen = () => {
+  const [currentGif, setCurrentGif] = useState(getRandomGif());
+  
   return (
 
     <View className='flex-1 bg-dark-grey justify-center items-center'>
-      <Image source={require('../assets/images/bear.gif')} style={styles.gif}/>
+      <Image source={currentGif} style={styles.gif}/>
     </View>
     
   );
