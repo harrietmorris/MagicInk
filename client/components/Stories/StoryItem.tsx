ff43e197bd2480ae246cf3a18fd7d939af8c8833
Regina Czech <regina.czech@yahoo.com>
2024-06-29 17:38:05 +0200

    removed styledPressables

--- client/components/Stories/StoryItem.tsx
+++ client/components/Stories/StoryItem.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { StoryType } from '@/types';
 import FavButton from '../buttons/favButton';
 import DeleteStoryBtn from '../buttons/DeleteStoryBtn';
-import { styled } from 'nativewind';
 
 interface StoryItemProps {
   story: StoryType;
@@ -11,24 +10,21 @@ interface StoryItemProps {
   buttonColor: string;
 }
 
-const StyledPressable = styled(Pressable);
-const StyledView = styled(View);
-
 const StoryItem = ({ story, handlePress, buttonColor }: StoryItemProps) => {
   return (
     <View>
-      <StyledPressable
+      <Pressable
         className={`w-[200px] h-[200px] bg-${buttonColor} aspect-square rounded-lg ml-5 p-2 flex items-center justify-center`}
         onPress={() => handlePress(story)}
       >
         <Text className='text-base text-black text-center text-lg font-bold'>{story.title}</Text>
-        <StyledView className='absolute top-2 left-2'>
+        <View className='absolute top-2 left-2'>
           <DeleteStoryBtn storyId={story.id} />
-        </StyledView>
+        </View>
         <View className='absolute top-2 right-2'>
           <FavButton storyId={story.id} />
         </View>
-      </StyledPressable>
+      </Pressable>
     </View>
   );
 };
