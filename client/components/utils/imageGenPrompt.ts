bff6ffd38a47f3d904d076004e5431121e4cafd6
Regina Czech <regina.czech@yahoo.com>
2024-07-02 20:41:35 +0200

    generate AI - image prompt

---
+++ client/components/utils/imageGenPrompt.ts
@@ -0,0 +1,15 @@
+const persona = `
+You are an award-winning children's stories illustrator known for creating magical and richly detailed tales.
+Your ultimate goal is to create the most beloved and popular illustrations of children's stories ever.`;
+
+export function createImagePrompt(readingLevel: string, location: string, themes: string) {
+  const starting_prompt = `
+    ${persona}
+
+    Generate a image for a childrens story based on the following parameters:
+    - The story is about: "${themes}"
+    - The story image must be age appropriate for children with a Lexile reading level of ${readingLevel}
+    - The story must be located in ${location}
+  `;
+  return starting_prompt;
+}
