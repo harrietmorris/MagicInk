35de5804e12f564099cab37412455fee367d3678
Regina Czech <regina.czech@yahoo.com>
2024-07-03 09:53:24 +0200

    fix: story item getStoryImage

--- client/components/Stories/StoryItem.tsx
+++ client/components/Stories/StoryItem.tsx
@@ -1,8 +1,9 @@
-import { View, Text, Pressable } from 'react-native';
-import React from 'react';
+import { View, Text, Pressable, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { StoryType } from '@/types';
 import FavButton from '../buttons/favButton';
 import DeleteStoryBtn from '../buttons/DeleteStoryBtn';
+import { getStoryImage } from '../utils/getStoryImage';
 
 interface StoryItemProps {
   story: StoryType;
@@ -11,14 +12,33 @@ interface StoryItemProps {
 }
 
 const StoryItem = ({ story, handlePress, buttonColor }: StoryItemProps) => {
+  const [imageURL, setImageURL] = useState<string>();
+
+  useEffect(() => {
+    if (story.id) {
+      (async () => {
+        const result = await getStoryImage(story.id);
+        setImageURL(result);
+      })();
+    }
+  }, []);
+
   return (
     <View>
       <Pressable
         // TODO: fix this hack to get light-orange to work
-        className={`w-[200px] h-[200px] bg-${buttonColor == 'light-orange'? '[#F4A662]': buttonColor} aspect-square rounded-lg ml-5 p-2 flex items-center justify-center`}
+        className={`w-[200px] h-[200px]
+          } aspect-square rounded-lg ml-5 p-2 flex items-center justify-center`}
         onPress={() => handlePress(story)}
       >
-        <Text className='text-base text-black text-center text-lg font-bold'>{story.title}</Text>
+        <Image
+          source={{
+            uri: imageURL,
+          }}
+          className='rounded-lg'
+          style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
+        />
+        <Text className='text-base text-black text-center text-lg font-bold bg-white'>{story.title}</Text>
         <View className='absolute top-2 left-2'>
           <DeleteStoryBtn storyId={story.id} />
         </View>
